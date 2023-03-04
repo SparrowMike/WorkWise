e0314b63f7762ba8d2d7f81585edd1ff07a5ae3d
SparrowMike <michal.m_wroblewski@hotmail.com>
2023-03-04 23:27:36 +0800

    feat: 🎸 replaced joke with quote

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './styles/css/style.css';
 import Clock from './components/Clock';
 import Joke from './components/Joke';
+import Quote from './components/Quote';
 
 function App() {
+
   return (
     <div className="App">
       <Clock/>
-      <Joke/> 
+      {/* <Joke/>  */}
+      <Quote/>
     </div>
   )
 }
