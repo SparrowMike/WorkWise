bfc7aeb7d2a252c91858e39622a388567a7d2ac6
SparrowMike <michal.m_wroblewski@hotmail.com>
2023-03-12 11:47:02 +0800

    feat: 🎸 loading bar animation

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import './../styles/sass/main.scss';
 
 import Clock from './components/Clock';
@@ -7,10 +7,13 @@ import Tracker from './components/Tracker';
 import Quote from './components/Quote';
 
 import { ThemeContext } from './context/ThemeContext';
+
 import ThemeToggle from './components/ThemeToggler';
+import ProgressBar from './components/ProgressBar';
 
 function Popup() {
   const { isDark, toggleTheme } = useContext(ThemeContext);
+  const [showElement, setShowElement] = useState(false);
 
   useEffect(() => {
     chrome.runtime.sendMessage({ type: 'LOAD_THEME' }, (response) => {
@@ -18,13 +21,31 @@ function Popup() {
     })
   }, []);
 
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setShowElement(true);
+    }, 200);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+
   return (
     <div className={`App ${isDark ? 'dark' : 'light'}`}>
-      <Clock/>
-      {/* <Joke/>  */}
-      <Quote/>
-      <Tracker/>
-      <ThemeToggle/>
+      {!showElement ? (
+        <ProgressBar />
+      ) : (
+        <>
+          <Clock />
+          {/* <Joke /> */}
+          <Quote />
+          <Tracker />
+          <ThemeToggle />
+        </>
+      )}
     </div>
   )
 }
---
+++ src/popup/components/ProgressBar.tsx
@@ -0,0 +1,11 @@
+function ProgressBar() {
+  return (
+    <div className="loading-progress-bar">
+      <span className="bar">
+        <span className="progress"></span>
+      </span>
+    </div>
+  )
+}
+
+export default ProgressBar
