3f52cdfaee8ee76a09c3880ef4edb9d4cd4222ae
SparrowMike <michal.m_wroblewski@hotmail.com>
2023-03-17 00:39:29 +0800

    fix: 🐛 on blob init check if its within the window

--- src/content/helpers/interactiveBlob.ts
+++ src/content/helpers/interactiveBlob.ts
@@ -22,12 +22,31 @@ export function interactiveBlob (
 
   chrome.storage.sync.get('blobPosition', (data) => {
     const blobPosition = JSON.parse(data.blobPosition || '{}');
+    
     if (blobPosition.left && blobPosition.top) {
-      container.style.left = blobPosition.left;
-      container.style.top = blobPosition.top;
+      // Get the size of the container
+      const containerWidth = container.offsetWidth;
+      const containerHeight = container.offsetHeight;
+  
+      // Get the size of the viewport
+      const viewportWidth = window.innerWidth;
+      const viewportHeight = window.innerHeight;
+  
+      // Calculate the maximum and minimum values for the left and top CSS properties
+      const maxLeft = viewportWidth - containerWidth;
+      const maxTop = viewportHeight - containerHeight;
+      const minLeft = 0;
+      const minTop = 0;
+  
+      // Adjust the maximum and minimum values for the left and top CSS properties
+      let newLeft = Math.max(minLeft, Math.min(maxLeft, parseFloat(blobPosition.left)));
+      let newTop = Math.max(minTop, Math.min(maxTop, parseFloat(blobPosition.top)));
+  
+      container.style.left = `${newLeft}px`;
+      container.style.top = `${newTop}px`;
     }
   });
-
+  
   if (container) {
     const userSelectStyle = window.getComputedStyle(container).userSelect;
 
